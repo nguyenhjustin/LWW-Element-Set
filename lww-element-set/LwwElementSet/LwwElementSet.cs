b88d3f70fcbfac5608ae1c62f2d201eb93e256ec
Justin Nguyen <nguyenhjustin@yahoo.com>
2018-07-06 23:03:23 -0700

    Stub functions for implementation

--- lww-element-set/LwwElementSet/LwwElementSet.cs
+++ lww-element-set/LwwElementSet/LwwElementSet.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using COLG = System.Collections.Generic;
 
 namespace LwwElementSet
 {
-  public class LwwElementSet
+  /// <summary>
+  /// An implementation of a LWW-Element-Set (Last-Write-Wins) 
+  /// CRDT (conflict-free replicated data type).
+  /// </summary>
+  public class LwwElementSet<T>
   {
+    private COLG.IDictionary<T, DateTime> m_addSet;
+    private COLG.IDictionary<T, DateTime> m_removeSet;
+
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    public LwwElementSet()
+    {
+      m_addSet = new COLG.Dictionary<T, DateTime>();
+      m_removeSet = new COLG.Dictionary<T, DateTime>();
+    }
+
+    /// <summary>
+    /// Adds an element to the set.
+    /// </summary>
+    /// <param name="element">The element to add.</param>
+    /// <param name="timestamp">The time of this operation.</param>
+    public void Add(
+      T element,
+      DateTime timestamp)
+    {
+
+    }
+
+    /// <summary>
+    /// Removes an element from the set.
+    /// </summary>
+    /// <param name="element">The element to remove.</param>
+    /// <param name="timestamp">The time of this operation.</param>
+    public void Remove(
+      T element,
+      DateTime timestamp)
+    {
+
+    }
+
+    /// <summary>
+    /// Looks up an element in the set.
+    /// </summary>
+    /// <param name="element">The element to lookup.</param>
+    /// <returns>True if the element is in the set; false if not.</returns>
+    public bool Lookup(T element)
+    {
+      return false;
+    }
   }
 }
