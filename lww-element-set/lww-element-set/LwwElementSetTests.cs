6c46bd3a532791ac06cb43388454fac0e2bfdc0c
Justin Nguyen <nguyenhjustin@yahoo.com>
2018-07-06 22:17:51 -0700

    Initial project files

---
+++ lww-element-set/lww-element-set/LwwElementSetTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lww_element_set
+{
+  [TestClass]
+  public class LwwElementSetTests
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
---
+++ lww-element-set/lww-element-set/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("lww-element-set")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lww-element-set")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a0304d9-0969-49aa-8036-cdbd2188e142")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
